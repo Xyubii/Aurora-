aec48a2fb5d9d9ad00045d39d0ce703175648047
Xyubii <68650338+Xyubii@users.noreply.github.com>
2026-06-03 03:12:07 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,14 @@
 const AuroraLibrary = {
-    "رياضيات": [
-        { title: "Analisi: Studio di Funzione", link: "https://www.matematicamente.it/appunti/analisi-matematica/studio-di-funzione/", desc: "شرح تفصيلي لدراسة الدوال والمشتقات لمنهج الماتوريتا." },
-        { title: "Formulario di Analisi", link: "https://www.youmath.it/formulario/analisi.html", desc: "ملخص شامل لكل قوانين التحليل الرياضي." }
+    "Math / Matematica / الرياضيات": [
+        { title: "Analisi di Funzione", link: "https://www.matematicamente.it/appunti/analisi-matematica/studio-di-funzione/", desc: "شرح شامل لدراسة الدوال - Analisi di Funzione" },
+        { title: "Integrali", link: "https://www.youmath.it/formulario/analisi.html", desc: "دليل التكاملات - Integrali" }
     ],
-    "فيزياء": [
-        { title: "Elettromagnetismo", link: "https://www.zanichelli.it/ricerca/prodotti/amaldi-per-i-licei-scientifici", desc: "موارد تعليمية رسمية حول الكهرومغناطيسية." },
-        { title: "Relatività e Quantistica", link: "https://www.skuola.net/fisica/fisica-moderna.html", desc: "شروحات مبسطة للفيزياء الحديثة." }
+    "Physics / Fisica / الفيزياء": [
+        { title: "Elettromagnetismo", link: "https://www.zanichelli.it/ricerca/prodotti/amaldi-per-i-licei-scientifici", desc: "الكهرومغناطيسية - Elettromagnetismo" },
+        { title: "Fisica Moderna", link: "https://www.skuola.net/fisica/fisica-moderna.html", desc: "الفيزياء الحديثة - Fisica Moderna" }
     ],
-    "أدب إيطالي": [
-        { title: "Pirandello e Svevo", link: "https://www.treccani.it/enciclopedia/luigi-pirandello/", desc: "تحليل أدبي ونقدي لأعمال الكبار في المنهج." },
-        { title: "Ungaretti e il 900", link: "https://www.skuola.net/letteratura-italiana-novecento/ungaretti-vita-poetica.html", desc: "دليل دراسي لشعر القرن العشرين." }
-    ],
-    "فلسفة": [
-        { title: "Nietzsche e Freud", link: "https://www.filosofico.net/nietzsche.htm", desc: "أرشيف فلسفي شامل لأهم الفلاسفة في المنهج." }
-    ],
-    "تاريخ": [
-        { title: "Il '900 e i Conflitti", link: "https://www.storiologia.it/", desc: "موسوعة تاريخية تغطي أحداث القرن العشرين." }
+    "Literature / Letteratura / الأدب": [
+        { title: "Pirandello", link: "https://www.treccani.it/enciclopedia/luigi-pirandello/", desc: "تحليل أدبي لبيرانديلو - Luigi Pirandello" },
+        { title: "Ungaretti", link: "https://www.skuola.net/letteratura-italiana-novecento/ungaretti-vita-poetica.html", desc: "شعر القرن العشرين - Giuseppe Ungaretti" }
     ]
 };
